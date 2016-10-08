2dbbb8d3b9185152c138d74128f487d589519f7d
Bobain <romain.burgot@gmail.com>
2016-10-08 14:37:02 +0200

    some nicer displays

--- app.R
+++ app.R
@@ -4,7 +4,8 @@ library(shinydashboard)
 ui <- dashboardPage(
   dashboardHeader(title = "CAH"),
   dashboardSidebar(
-    fileInput("file1", "Choose CSV File",
+    fileInput("file1", "Choose CSV File", 
+              multiple = FALSE,
               accept = c(
                 "text/csv",
                 "text/comma-separated-values,text/plain",
@@ -16,8 +17,17 @@ ui <- dashboardPage(
       menuItem("CAH groups", tabName = "cahresult")
     ),
     sliderInput("nbGroups", "Number of groups:",
-                min = 1, max = 100, value = 3, step = 1 # TODO dynamic depending on number of rows
-    )
+                min = 2, max = 100, value = 3, step = 1 # TODO dynamic depending on number of rows
+    ),
+    checkboxInput("doScaleData", "Center reduce variables:", value = FALSE, width = NULL),
+    selectizeInput("cahMethod", "Choose your method:", 
+                   c("ward.D", "ward.D2", "single", "complete", "average", "mcquitty", "median", "centroid"), 
+                   selected = "complete", multiple = FALSE,
+                   options = NULL),
+    selectizeInput("distance", "Choose your distance:", 
+                   c("euclidean", "maximum", "manhattan", "canberra", "binary", "minkowski"),
+                   selected = "euclidean", multiple = FALSE,
+                   options = NULL)
   ),
   dashboardBody(
     tabItems(
@@ -50,7 +60,7 @@ testNoFile <- function(f, data){
     f(data)
   }
   else {
-    text(0,0, "please input a file")
+    stop("Please input a csv file")
   }
 }
 
@@ -59,15 +69,21 @@ server <- function(input, output) {
   dataInput <- reactive({
     inFile <- input$file1
     
-    if (is.null(inFile))
-      return(NULL)
-    
     results = list()
+    if (is.null(inFile)){
+      results$df <- read.csv('./data/donclassif.txt',header=T,sep=";")
+    }
+    else {
+      results$df <- read.csv(inFile$datapath,header=T,sep=";")
+    }
     
-    results$df <- read.csv(inFile$datapath,header=T,sep=";")
+    if (input$doScaleData)
+      data2work = scale(results$df)
+    else
+      data2work = results$df    
     
-    matD <- dist(results$df)
-    results$cahS <- hclust(matD)
+    matD <- dist(data2work, method= input$distance)
+    results$cahS <- hclust(matD, method = input$cahMethod)
     
     results$rescah <- cutree(results$cahS,k=input$nbGroups)
     
@@ -77,18 +93,21 @@ server <- function(input, output) {
   output$plotDendo <- renderPlot({
     f <- function(data){
       plot(data$cahS)
+      # TODO : abline(data$cah$height[input$nbGroups], 0, col = "red")
     }
     testNoFile(f, dataInput())
   })
   output$plotHeight <- renderPlot({
     f <- function(data){
-      plot(sort(data$cah$height,dec=T)[1:(input$nbGroups+5)],type="h")
+      plot(2:(input$nbGroups+6), sort(data$cah$height,dec=T)[1:(input$nbGroups+5)],type="h",ylab="height",xlab="nbGroups", main='Height depending on number of groups')
+      points(input$nbGroups, sort(data$cah$height,dec=T)[input$nbGroups-1], type="p", col = "red", cex=3)
     }
     testNoFile(f, dataInput())
   })
   output$plotGroups <- renderPlot({
     f3 <- function(data){
-      plot(data$df,col=data$rescah)
+      names_ <- names(data$df)
+      plot(data$df[, 1:2],col=data$rescah,xlab = names_[1],ylab = names_[2], main = "Groups on first two variables")
     }
     testNoFile(f3, dataInput())
   })
